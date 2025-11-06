4150b2a5442c76ab1a20f184b1c0d4636132198e
Yafiakmal <yafiakmal45@gmail.com>
2025-11-06 14:19:58 +0700

    main.go berubah

--- URL-SHORTENER/cmd/main.go
+++ URL-SHORTENER/cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"log"
 	"time"
-	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -25,13 +25,13 @@ func main() {
 
 	server := gin.Default()
 
- server.Use(cors.New(cors.Config{
-    AllowOrigins:  []string{"*"}, // atau ganti dengan domain frontend kamu
-    AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-    AllowHeaders:  []string{"Origin", "Content-Type", "Accept"},
-    ExposeHeaders: []string{"Content-Length"},
-    MaxAge:        12 * time.Hour,
-  }))
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"}, // atau ganti dengan domain frontend kamu
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders: []string{"Content-Length"},
+		MaxAge:        12 * time.Hour,
+	}))
 
 	server.POST("/shortener", func(ctx *gin.Context) {
 		handler.AddUrlHandler(ctx, db)
@@ -40,6 +40,9 @@ func main() {
 		handler.GetAllUrlHandler(ctx, db)
 	})
 	server.GET("/:name", func(ctx *gin.Context) {
+		handler.Redirect(ctx, db)
+	})
+	server.DELETE("/:id", func(ctx *gin.Context) {
 		handler.Redirect(ctx, db)
 	})
 	server.Run(":80")
